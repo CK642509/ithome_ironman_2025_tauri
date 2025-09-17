d74f07b55fd931d5fe592face395e7824260f569
johnlin <john830110@gmail.com>
2025-09-18 00:02:51 +0800

    chore: android init

---
+++ src-tauri/gen/android/app/src/main/java/com/ck642509/ithome_ironman_2025_tauri/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ck642509.ithome_ironman_2025_tauri
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}

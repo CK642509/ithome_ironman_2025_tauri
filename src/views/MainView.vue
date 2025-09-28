51b412424b32ce616fa9e6affe829c44662776d7
johnlin <john830110@gmail.com>
2025-09-28 11:17:58 +0800

    fix; 修正路徑錯誤

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -77,7 +77,7 @@ async function createChatWindow() {
         <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
       </a>
       <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+        <img src="../assets/vue.svg" class="logo vue" alt="Vue logo" />
       </a>
     </div>
     <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
